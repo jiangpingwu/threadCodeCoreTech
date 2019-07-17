b037d7a39b048d9a251053a38e06744ee20b7493
alan <252842510@qq.com>
2019-07-17 15:04:53 +0800

    Thread

---
+++ src/chapterone/seven/three/MyThread.java
@@ -0,0 +1,23 @@
+package chapterone.seven.three;
+
+/**
+ * @ClassName: MyThread
+ * @description: TODO
+ * @author: alan
+ * @date: 2019-07-17 14:57
+ * @version: V1.0
+ **/
+public class MyThread extends Thread {
+
+    public void run() {
+        super.run();
+        for(int i = 0;i <5000000;i++) {
+            if(this.interrupted()) {
+                System.out.println("已经是停止的状态! 我要退出了！");
+                break;
+            }
+            System.out.println("i="+(i+1));
+        }
+    }
+
+}
---
+++ src/chapterone/seven/three/Run.java
@@ -0,0 +1,25 @@
+package chapterone.seven.three;
+
+/**
+ * @ClassName: Run
+ * @description: TODO
+ * @author: alan
+ * @date: 2019-07-17 14:57
+ * @version: V1.0
+ **/
+public class Run {
+
+    public static void main(String[] args) {
+        try {
+            MyThread thread= new MyThread();
+            thread.start();
+            Thread.sleep(2000);
+            thread.interrupt();
+        } catch (InterruptedException e) {
+            System.out.println("main catch");
+            e.printStackTrace();
+        }
+
+        System.out.println("end");
+    }
+}
