e9f550787928372aae9322f5f770a5c055fddfcb
alan <252842510@qq.com>
2019-07-17 15:32:19 +0800

    Thread

--- src/chapterone/seven/three/MyThread.java
+++ src/chapterone/seven/three/MyThread.java
@@ -11,12 +11,19 @@ public class MyThread extends Thread {
 
     public void run() {
         super.run();
-        for(int i = 0;i <5000000;i++) {
-            if(this.interrupted()) {
-                System.out.println(this.getName()+" 已经是停止的状态! 我要退出了！");
-                break;
+        try {
+            for(int i = 0;i <5000000;i++) {
+                if(this.interrupted()) {
+                    System.out.println(this.getName()+" 已经是停止的状态! 我要退出了！");
+                    throw new InterruptedException();
+                    //break;
+                }
+                System.out.println(this.getName()+" i="+(i+1));
             }
-            System.out.println(this.getName()+" i="+(i+1));
+            System.out.println("我在for下面");
+        } catch (Exception e) {
+            System.out.println("进入MyThread.java类run方法中的catch");
+            e.printStackTrace();
         }
     }
 
