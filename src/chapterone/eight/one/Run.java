50680b926c06ca85ce94cf1dd6e8845aff55ef05
alan <252842510@qq.com>
2019-07-24 09:37:44 +0800

    Thread

--- src/chapterone/eight/one/Run.java
+++ src/chapterone/eight/one/Run.java
@@ -22,13 +22,15 @@ public class Run {
 
             //B段
             thread.resume();
+            System.out.println("B= " + System.currentTimeMillis() + " i= "+thread.getI());
             Thread.sleep(5000);
+            System.out.println("B= " + System.currentTimeMillis() + " i= "+thread.getI());
 
             //C段
             thread.suspend();
-            System.out.println("B= " + System.currentTimeMillis() + " i= "+thread.getI());
+            System.out.println("C= " + System.currentTimeMillis() + " i= "+thread.getI());
             Thread.sleep(5000);
-            System.out.println("B= " + System.currentTimeMillis() + " i= "+thread.getI());
+            System.out.println("C= " + System.currentTimeMillis() + " i= "+thread.getI());
         }catch (Exception e){
             e.printStackTrace();
         }
